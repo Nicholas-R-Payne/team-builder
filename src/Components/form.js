ecb22bada37e760582cabbfc5becb4f4fa332f76
DarkedgeTH <darkedgeth@gmail.com>
2022-02-22 14:31:28 -0500

    started setting up form structure

--- src/Components/form.js
+++ src/Components/form.js
@@ -1,2 +1,35 @@
 import React from 'react'
 
+const form = (props) => {
+    return (
+        <form>
+            <label>Name
+                <input
+                    placeholder='Enter your name here!'
+                    value={values.name}
+                    name='name'
+                    onChange={handleChange}
+                />
+            </label>
+            <label>Email
+                <input
+                    placeholder='Enter your email here!'
+                    value={values.email}
+                    name='email'
+                    onChange={handleChange}
+                />
+            </label>
+            <label>Role
+                <input
+                    placeholder='Enter your role here!'
+                    value={values.role}
+                    name='role'
+                    onChange={handleChange}
+                />
+            </label>
+            <input type='submit' value='Create your team!' />
+        </form>
+    )
+}
+
+export default form;
