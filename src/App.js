acd7f02ae4a6886d59cf4e6d58ef5694a57dbe7a
DarkedgeTH <darkedgeth@gmail.com>
2022-02-22 14:38:47 -0500

    imported React and Form in App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
-import './App.css';
+import React, {useState} from 'react';
+
+import Form from './Components/Form'
 
 function App() {
+  const {members, setMembers} = useState({ name: '', email: '', role: ''})
   return (
     <div className="App">
       <h1>Team Builder App</h1>
